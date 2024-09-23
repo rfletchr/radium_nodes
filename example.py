a4479d7a47ed75d6b8dbfb16a7808a8586466694
rlfetchr <rcsfletcher@protonmail.com>
2024-09-23 19:35:50 +0100

    update example image and readme

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 Drag from ports to edges to make connections.
 Edit edges by dragging them (not their ports)
 
-Shift-Drag nodes to duplicate them.
+Shift-Drag nodegraph to duplicate them.
 Ctrl-Drag connections to add corners.
 
 Middle Drag background to pan view.
@@ -13,32 +13,38 @@ e.g.
     export QT_QPA_PLATFORM=xcb python example.py
 
 """
-from PySide6 import QtWidgets
-from radium.nodegraph import NodeGraphView, NodeGraphViewEventFilter
-from radium.nodegraph import NodeGraphSceneController
-from radium.nodegraph import Node
+
+import os
+from PySide6 import QtWidgets, QtGui, QtCore, QtSvg
+from radium.nodegraph import NodeGraphView
+from radium.nodegraph import NodeGraphController
+from radium.nodegraph import NodePrototype, PortPrototype
+
+os.environ["QT_SCALE_FACTOR"] = "2"
+
 
 app = QtWidgets.QApplication([])
-scene = QtWidgets.QGraphicsScene()
-controller = NodeGraphSceneController(scene)
-
-node = Node("ReadImage", [], ["image"])
-node.setPos(0, 0)
-scene.addItem(node)
-
-node2 = Node("ReadImage2", [], ["image"])
-node2.setPos(0, 0)
-scene.addItem(node2)
-
-node2 = Node("Merge", ["image1", "image2"], ["image"])
-node2.setPos(0, 100)
-scene.addItem(node2)
-scene.setSceneRect(-1000, -1000, 2000, 2000)
-
-
 view = NodeGraphView()
-event_filter = NodeGraphViewEventFilter()
-view.viewport().installEventFilter(event_filter)
-view.show()
-view.setScene(scene)
+
+controller = NodeGraphController()
+view.setScene(controller.scene)
+
+main_window = QtWidgets.QMainWindow()
+main_window.menuBar().addAction(controller.undo_stack.createUndoAction(app))
+main_window.menuBar().addAction(controller.undo_stack.createRedoAction(app))
+main_window.setCentralWidget(view)
+main_window.show()
+
+merge_prototype = NodePrototype(
+    "Merge",
+    inputs=(PortPrototype("input1", "image"), PortPrototype("input2", "image")),
+    outputs=(PortPrototype("output", "image"),),
+    parameters=tuple(),
+)
+controller.createNode(merge_prototype)
+controller.createNode(merge_prototype)
+controller.createNode(merge_prototype)
+controller.createBackdrop("Backdrop")
+
+
 app.exec()
